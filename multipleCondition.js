c6e6012ea382195154a6fa7154d0b32a05acd9d6
Al Nahian Ark <63243119+arknahian@users.noreply.github.com>
2020-12-21 00:07:09 +0600

    finish

---
+++ multipleCondition.js
@@ -0,0 +1,42 @@
+var didYouGetJob = true;
+var savingAmount = 80000;
+
+if (didYouGetJob == true && savingAmount > 500000) {
+    console.log("Ok Let's find your feyonce")
+}
+else {
+    console.log("there is no one in your faith");
+}
+
+var didYouGetJob2 = true;
+var savingAmount2 = 800000;
+if (didYouGetJob2 == true && savingAmount2 > 500000) {
+    console.log("Ok Then we are finding someone for you")
+}
+else{
+    console.log("Ops! There is no one for yourself")
+}
+
+var didYouGetJob3 = false;
+var savingAmount3 = 800000;
+if (didYouGetJob3 == true || savingAmount3 > 500000) {
+    console.log("Let's find someone for you")
+}
+else{
+    console.log("Nope!")
+}
+
+
+
+
+var jobPaiso = true;
+var savingAmount4 = 70000;
+if (jobPaiso == true && savingAmount4 > 500000) {
+    console.log("Ok")
+}
+else if (jobPaiso == true) {
+    console.log("wait some days")
+}
+else{
+    console.log("No Way")
+}
